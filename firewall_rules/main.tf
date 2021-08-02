a38e5a85f150273a1886bce508cec565cd32b951
Stefan Andrei <andreistefanciprian@gmail.com>
2021-08-02 11:17:31 +0100

    add https

--- firewall_rules/main.tf
+++ firewall_rules/main.tf
@@ -34,7 +34,7 @@ resource "google_compute_firewall" "default" {
 
   allow {
     protocol = "tcp"
-    ports    = ["80", "8080"]
+    ports    = ["80", "8080", "433"]
   }
 
   source_tags = ["web"]
